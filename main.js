94645c25ced84f8f1cd5cec3dd5d306de6cffb98
Vishal Deshwal <inpexel@gmail.com>
2022-07-26 21:49:02 +0530

    calulator for 3d renders

---
+++ main.js
@@ -0,0 +1,9 @@
+const whiteBg = document.querySelector('.whiteBg');
+const creative = document.querySelector('.creative');
+const headButtons1 = document.querySelector('.headButtons button:first-of-type');
+const headButtons2 = document.querySelector('.headButtons button:last-of-type');
+
+
+function isBtnActive(button){
+    button.classList = ('.btn-selected') 
+}
